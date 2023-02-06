adde40621285a4c0d0ef0670a1e151b5335bb4d5
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2023-02-06 17:44:10 +0900

    Update few_shot_adapt.sh

--- few_shot_adapt.sh
+++ few_shot_adapt.sh
@@ -4,19 +4,19 @@ do
     do
         CUDA_VISIBLE_DEVICES=4 python target_finetune.py --dataset $1 \
         --pretrain SHOT --adapt $2 --few_shot $shot \
-        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_fixedval_robust_'$seed --lr $3 --source 0 --SAM --seed $seed &
+        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_val_'$seed --lr $3 --source 0 --SAM --seed $seed &
 
         CUDA_VISIBLE_DEVICES=5 python target_finetune.py --dataset $1 \
         --pretrain SHOT --adapt $2 --few_shot $shot \
-        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_fixedval_robust_'$seed --lr $3 --source 1 --SAM --seed $seed &
+        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_val_'$seed --lr $3 --source 1 --SAM --seed $seed &
 
         CUDA_VISIBLE_DEVICES=6 python target_finetune.py --dataset $1 \
         --pretrain SHOT --adapt $2 --few_shot $shot \
-        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_fixedval_robust_'$seed --lr $3 --source 2 --SAM --seed $seed &
+        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_val_'$seed --lr $3 --source 2 --SAM --seed $seed &
 
         CUDA_VISIBLE_DEVICES=7 python target_finetune.py --dataset $1 \
         --pretrain SHOT --adapt $2 --few_shot $shot \
-        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_fixedval_robust_'$seed --lr $3 --source 3 --SAM --seed $seed &
+        --work_dir 'SHOT_'$1'_'$2'_'$shot'shot_SAM_val_'$seed --lr $3 --source 3 --SAM --seed $seed &
 
         wait
     done
