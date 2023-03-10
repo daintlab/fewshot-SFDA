6373af674a89fd0ccd5e70f8501d12cf194958be
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2023-03-10 17:36:05 +0900

    Update target_finetune.py

--- target_finetune.py
+++ target_finetune.py
@@ -10,22 +10,24 @@ from torch.utils.data import DataLoader
 from collections import OrderedDict
 from sam import SAM
 import matplotlib.pyplot as plt
-from model import ERM,SHOT
-from dataset import get_target_dataset
+from model import ERM,SHOT,IMGNET,SHOT_fe
+# from dataset2 import get_target_dataset as get_target_dataset_robust
+from dataset import get_target_dataset 
 from data_loader import InfiniteDataLoader
 import utils
 import warnings
-
+from sklearn.metrics import confusion_matrix
+import pickle
 warnings.filterwarnings(action='ignore')
 
 parser = argparse.ArgumentParser(description='Adapt and test on target domain')
 parser.add_argument('--data_dir', default='/data/domainbed',type=str,help='Data directory')
 parser.add_argument('--dataset', default='PACS',type=str,
-                    choices=['PACS','office_home','terra_incognita','VLCS','office31'],help='Data directory')
+                    choices=['PACS','office_home','terra_incognita','VLCS','office31','VISDA-C'],help='Data directory')
 parser.add_argument('--work_dir', default='./result',type=str,help='Working directory')
 parser.add_argument('--source', default=None,type=int,help='Index of source domain')
 parser.add_argument('--target', default=None,type=int,help='Index of target domain')
-parser.add_argument('--pretrain', default='SHOT',type=str,choices=['SHOT'],help='Source pretrain strategy')
+parser.add_argument('--pretrain', default='SHOT',type=str,choices=['SHOT','SHOT_LPFT','IMGNET'],help='Source pretrain strategy')
 
 parser.add_argument('--adapt', default=None,type=str,choices=['cls','clsBN','BN','feat','all'],help='Fine-tuning method')
 parser.add_argument('--adapt_step', default=1000,type=int,help='Adaptation step')
@@ -48,11 +50,17 @@ parser.add_argument('--rho', default=0.05,type=float,help='SAM rho')
 parser.add_argument('--flood', type=float,default=None,help='loss flooding')
 parser.add_argument('--robust',action='store_true',default=False,help='Robust validation')
 parser.add_argument('--seed', default=0,type=int,help='Random seed')
+parser.add_argument('--oda_seed', default=2020,type=int,help='oda split Random seed')
+parser.add_argument('--subset',action='store_true',default=False,help='partial labeled dataset')
+parser.add_argument('--imbalance',action='store_true',default=False)
+parser.add_argument('--LPFT_use_best',action='store_true',default=False)
 args = parser.parse_args()
 
 def train_one_step(model,criterion,optimizer,x,y,args):
+# def train_one_step(model,criterion,optimizer,x,y,params,args):
     utils.freeze_proper_param(model,args)
     x,y = x.cuda(),y.cuda()
+    # x,y,params = x.cuda(),y.cuda(),params.cuda()
     
     # mixup
     if args.mixup:
@@ -69,7 +77,9 @@ def train_one_step(model,criterion,optimizer,x,y,args):
             loss = utils.mixup_criterion(criterion, mixed_output, y_a, y_b, lambd)
     else:
         output = model(x)
+        # output,c_output = model(x,False)
         loss = criterion(output,y)
+        # loss += torch.nn.MSELoss()(c_output,params)
     
     if args.SAM:
         loss.backward()
@@ -83,6 +93,9 @@ def train_one_step(model,criterion,optimizer,x,y,args):
                 utils.mixup_criterion(criterion, model(mixed_x), y_a, y_b, lambd).backward()
         else:
             criterion(model(x), y).backward()
+            # output,c_output = model(x,False)
+            # (criterion(output, y)+torch.nn.MSELoss()(c_output,params)).backward()
+            
         optimizer.second_step(zero_grad=True)
     else:
         optimizer.zero_grad()
@@ -92,7 +105,7 @@ def train_one_step(model,criterion,optimizer,x,y,args):
     acc,_= utils.accuracy(output, y, topk=(1,5))
     return loss.item(), acc
 
-def test(model,criterion,loader):
+def test(model,criterion,loader,flag=False):
     model.eval()
     test_loss = utils.AverageMeter()
     test_acc = utils.AverageMeter()
@@ -101,12 +114,24 @@ def test(model,criterion,loader):
             x,y = x.cuda(), y.cuda()
             output = model(x)
             loss = criterion(output, y)
-            
-            acc,_= utils.accuracy(output, y, topk=(1,5))
+            if flag:
+                if i == 0:
+                    total_pred = torch.argmax(output.detach().cpu(), 1)
+                    total_label = y.detach().cpu()
+                else:
+                    total_pred = torch.cat((total_pred, torch.argmax(output.detach().cpu(), 1)))
+                    total_label = torch.cat((total_label, y.detach().cpu()))
+            else:
+                acc,_= utils.accuracy(output, y, topk=(1,5))
+                test_acc.update(acc[0].item(), x.size(0))
             test_loss.update(loss.item(), x.size(0))
-            test_acc.update(acc[0].item(), x.size(0))
-    
-    return test_loss.avg, test_acc.avg
+    if flag:
+        cm = confusion_matrix(total_label, total_pred)
+        per_class_acc = cm.diagonal()/cm.sum(axis=1) * 100
+        test_acc = per_class_acc.mean()                    
+        return test_loss.avg, test_acc, per_class_acc
+    else:
+        return test_loss.avg, test_acc.avg
 
 def train_on_target(args):
     save_path = os.path.join(args.work_dir, f'target{args.target}')
@@ -114,7 +139,20 @@ def train_on_target(args):
     
     # data
     train_dataset, val_dataset, test_dataset, target_domain = get_target_dataset(args)
-    num_classes = len(test_dataset.underlying_dataset.classes)
+    if args.subset:
+        if args.dataset == 'office_home':
+            num_classes = 25
+        if args.dataset == 'office31':
+            num_classes = 10
+        if args.dataset == 'VISDA-C':
+            num_classes = 6
+    elif args.imbalance:
+        if args.dataset == 'office_home':
+            num_classes = 65
+        if args.dataset == 'VISDA-C':
+            num_classes = 12
+    else:
+        num_classes = len(test_dataset.underlying_dataset.classes)
 
     if len(train_dataset) < args.batch_size:
         multiplier = int(args.batch_size / len(train_dataset)) + 1
@@ -132,9 +170,13 @@ def train_on_target(args):
     
     # model
     if args.pretrain == 'SHOT':
-        model = SHOT(ckpts=args.ckpts, dataset=args.dataset).cuda()
+        model = SHOT(ckpts=args.ckpts, dataset=args.dataset, subset=args.subset).cuda()
+    elif args.pretrain == 'SHOT_LPFT':
+        model = SHOT_fe(ckpts=args.ckpts, dataset=args.dataset, subset=args.subset).cuda()
     else:
-        raise NotImplementedError
+        # raise NotImplementedError
+        print('Source model is pretrained with ImageNet')
+        model = IMGNET(ckpts=args.ckpts, dataset=args.dataset).cuda()
 
     criterion = nn.CrossEntropyLoss(label_smoothing=args.label_smoothing)
     
@@ -170,7 +212,9 @@ def train_on_target(args):
     else:
         for step in range(args.adapt_step):
             x,y = next(train_iterator)
+            # x,y,params = next(train_iterator)
             loss,acc = train_one_step(model,criterion,optimizer,x,y,args)
+            # loss,acc = train_one_step(model,criterion,optimizer,x,y,params,args)
             train_loss.update(loss,x.size(0))
             train_acc.update(acc[0].item(),x.size(0))
             loss_plot.append(loss)
@@ -192,7 +236,10 @@ def train_on_target(args):
                 result['val_acc'] = val_acc
                 
                 # Test on target test set
-                test_loss,test_acc = test(model,criterion,test_loader)
+                if args.dataset == 'VISDA-C' or args.dataset == 'VLCS' or args.dataset == 'terra_incognita' or args.imbalance:
+                    test_loss,test_acc,test_per_class_acc = test(model,criterion,test_loader,flag=True)
+                else:
+                    test_loss,test_acc = test(model,criterion,test_loader)
                 result['test_loss'] = test_loss
                 result['test_acc'] = test_acc
                 
@@ -207,11 +254,15 @@ def train_on_target(args):
                 if best_loss > val_loss:
                     best_loss = val_loss
                     seleted_acc = test_acc
-
+                    if args.dataset == 'VISDA-C' or args.dataset == 'VLCS' or args.dataset == 'terra_incognita' or args.imbalance:
+                        selected_per_class_acc = test_per_class_acc
+                    torch.save(model.state_dict(),os.path.join(save_path,'best_ckpt.pth'))
+                    
                 # Best acc
                 if test_acc > best_acc:
                     best_acc = test_acc
-        
+                    if args.dataset == 'VISDA-C' or args.dataset == 'VLCS' or args.dataset == 'terra_incognita' or args.imbalance:
+                        best_per_class_acc = test_per_class_acc
         # 충분히 수렴하는지 확인하기 위한 시각화 코드
         # plt.plot(loss_plot, label='loss')
         # plt.legend()
@@ -226,62 +277,72 @@ def train_on_target(args):
 
         # Model save
         torch.save(model.to('cpu').state_dict(),os.path.join(save_path,'ckpt.pth'))
-    
-    return best_acc, seleted_acc, result['test_acc']
+    if args.dataset == 'VISDA-C' or args.dataset == 'VLCS' or args.dataset == 'terra_incognita' or args.imbalance:
+        return best_acc, seleted_acc, result['test_acc'], best_per_class_acc, selected_per_class_acc, test_per_class_acc
+    else:
+        return best_acc, seleted_acc, result['test_acc']
 
 def get_ckpt(args):
     domain_dict = {
                 'office_home' : ['A','C','P','R'],
                 'PACS' : ['A','C','P','S'],
                 'VLCS' : ['C','L','S','V'],
-                'office31' : ['A','D','W']
+                'office31' : ['A','D','W'],
+                'terra_incognita' : ['L100','L38','L43','L46'],
+                'VISDA-C' : ['T','V']
             }
 
     if args.pretrain == 'SHOT':
         if args.adapt is not None:
             if args.seed == 0:
-                fol_name = 'source_seed2020'
+                if args.subset:
+                    fol_name = f'source_odaseed{args.oda_seed}_seed2020/oda'
+                elif args.imbalance:
+                    fol_name = f'source_imbalance_seed2020/uda'
+                else:
+                    fol_name = 'source_seed2020/uda'
             elif args.seed == 1:
-                fol_name = 'source_seed2021'
+                if args.subset:
+                    fol_name = f'source_odaseed{args.oda_seed}_seed2021/oda'
+                elif args.imbalance:
+                    fol_name = f'source_imbalance_seed2021/uda'
+                else:
+                    fol_name = 'source_seed2021/uda'
             elif args.seed == 2:
-                fol_name = 'source_seed2022'
+                if args.subset:
+                    fol_name = f'source_odaseed{args.oda_seed}_seed2022/oda'
+                elif args.imbalance:
+                    fol_name = f'source_imbalance_seed2022/uda'
+                else:
+                    fol_name = 'source_seed2022/uda'
             
-            root_dir = f'/nas/datahub/SFDA/shot_src_pretrained/ckps/{fol_name}/uda'
+            root_dir = f'/nas/home/tmddnjs3467/domain-generalization/SHOT/object/ckps/{fol_name}'
             
             ckpts = {}
             ckpts['netF'] = os.path.join(root_dir,f'{args.dataset}/{domain_dict[args.dataset][args.source]}/source_F.pt')
             ckpts['netB'] = os.path.join(root_dir,f'{args.dataset}/{domain_dict[args.dataset][args.source]}/source_B.pt')
             ckpts['netC'] = os.path.join(root_dir,f'{args.dataset}/{domain_dict[args.dataset][args.source]}/source_C.pt')
         
-        # 여기는 특정 모델 Test하고 싶을 때 사용하는 코드, test하고 싶은 모델 load하는 부분이니까 바꿔사용하면됨
-        # else:
-        #     if args.seed == 0:
-        #         fol_name = f'seed2020'
-        #         seed = 2020
-        #     elif args.seed == 1:
-        #         fol_name = f'seed2021'
-        #         seed = 2021
-        #     elif args.seed == 2:
-        #         fol_name = f'seed2022'
-        #         seed = 2022
-            
-        #     root_dir = f'/nas/home/tmddnjs3467/domain-generalization/SHOT/object/ckps/target_samesplit_{fol_name}/uda'
-        #     pair = domain_dict[args.dataset][args.source] + domain_dict[args.dataset][args.target]
-
-        #     ckpts = {}
-        #     ckpts['netF'] = os.path.join(root_dir,f'{args.dataset}/{pair}/target_F_par_0.3.pt')
-        #     ckpts['netB'] = os.path.join(root_dir,f'{args.dataset}/{pair}/target_B_par_0.3.pt')
-        #     ckpts['netC'] = os.path.join(root_dir,f'{args.dataset}/{pair}/target_C_par_0.3.pt')
-        
-        #     root_dir = f'/nas/home/tmddnjs3467/domain-generalization/CoWA-JMDS/ckps/source/uda'
-        #     ckpts = {}
-        #     ckpts['netF'] = os.path.join(root_dir,f'{args.dataset}/{domain_dict[args.dataset][args.source]}/source_F_{seed}.pt')
-        #     ckpts['netB'] = os.path.join(root_dir,f'{args.dataset}/{domain_dict[args.dataset][args.source]}/source_B_{seed}.pt')
-        #     ckpts['netC'] = os.path.join(root_dir,f'{args.dataset}/{domain_dict[args.dataset][args.source]}/source_C_{seed}.pt')
-        
-    else:
-        raise NotImplementedError
+    elif args.pretrain == 'SHOT_LPFT':
+        if args.subset:
+            root_dir = f'./D_fine_tuning_output/{args.dataset}/SHOT_{args.dataset}_cls_{args.few_shot}shot_SAM_fixedval_lr_1e-3_subset_2020_0'
+        elif args.imbalance:
+            root_dir = f'./D_fine_tuning_output/{args.dataset}/SHOT_{args.dataset}_cls_{args.few_shot}shot_SAM_fixedval_imbalance_lr_1e-3_0'
+        else:
+            root_dir = f'./D_fine_tuning_output/{args.dataset}/SHOT_{args.dataset}_cls_{args.few_shot}shot_SAM_fixedval_lr_1e-3_0'
+  
+        if args.LPFT_use_best:
+            ckpts = os.path.join(root_dir,f'source{args.source}/target{args.target}/best_ckpt.pth')        
+        else:
+            ckpts = os.path.join(root_dir,f'source{args.source}/target{args.target}/ckpt.pth')
     
+    else:
+        # raise NotImplementedError        
+        # ckpts = None
+        root_dir = f'/nas/home/tmddnjs3467/domain-generalization/source_free_DA/D_fine_tuning_output/{args.dataset}/IMGNET_office_home_cls_1shot_SAM_fixedval_lr_1e-3_{args.seed}'
+        ckpts = os.path.join(root_dir,f'source{args.source}/target{args.target}/ckpt.pth')
+        print('Source model is pretrained with ImageNet')
+
     return ckpts
 
 def main():
@@ -294,25 +355,40 @@ def main():
     # Set seed
     utils.set_seed(args.seed)
 
-    assert args.dataset in ['PACS','office_home','terra_incognita','VLCS','office31']
+    assert args.dataset in ['PACS','office_home','terra_incognita','VLCS','office31','VISDA-C']
     last_target_acc = {}
     selected_target_acc = {}
     best_target_acc = {}
     if args.dataset == 'office31':
         num_d = 3
+    elif args.dataset == 'VISDA-C':
+        num_d = 2
     else:
         num_d = 4
     for target in range(num_d):
         if target == args.source :
             continue
+        if args.dataset == 'office_home' and args.imbalance and target == 0:
+            continue
         args.target = target
         args.ckpts = get_ckpt(args)
         print(f"{args.dataset} source {args.source} -> target {target}")
-        best_acc,selected_acc,last_acc = train_on_target(args)
+        if args.dataset == 'VISDA-C' or args.dataset == 'VLCS' or args.dataset == 'terra_incognita' or args.imbalance :
+            class_per_acc = {}
+            best_acc,selected_acc,last_acc,best_cls_acc,selected_cls_acc,last_cls_acc = train_on_target(args)
+        else:
+            best_acc,selected_acc,last_acc = train_on_target(args)
         
         last_target_acc[f'source{args.source}@target{target}'] = last_acc
         selected_target_acc[f'source{args.source}@target{target}'] = selected_acc
         best_target_acc[f'source{args.source}@target{target}'] = best_acc
+        if args.dataset == 'VISDA-C' or args.dataset == 'VLCS' or args.dataset == 'terra_incognita' or args.imbalance:
+            class_per_acc['best'] = best_cls_acc   
+            class_per_acc['selected'] = selected_cls_acc   
+            class_per_acc['last'] = last_cls_acc   
+            with open(os.path.join(args.work_dir,'class_per_acc.pkl'),'wb') as f:
+                pickle.dump(class_per_acc, f)
+    
     # 결과 aggregate
     best_target_acc = OrderedDict(sorted(best_target_acc.items()))
     selected_target_acc = OrderedDict(sorted(selected_target_acc.items()))
