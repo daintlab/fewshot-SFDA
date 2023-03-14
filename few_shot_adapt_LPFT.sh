f96d863190aa8f1656ab4a609caba570a65fe7ad
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2023-03-14 16:42:05 +0900

    Update few_shot_adapt_LPFT.sh

--- few_shot_adapt_LPFT.sh
+++ few_shot_adapt_LPFT.sh
@@ -4,14 +4,7 @@ do
     do
         for seed in 0 1 2
         do
-            if [ $1 = 'VISDA-C' ]
-            then
-
-                CUDA_VISIBLE_DEVICES=$3 python target_finetune.py --dataset $1 \
-                --pretrain SHOT_LPFT --adapt $2 --few_shot $shot \
-                --work_dir 'SHOT_LPFT_'$1'_'$2'_'$shot'shot_SAM_fixedval_lr_'$lr'_'$seed --lr $lr --source 0 --SAM --seed $seed &
-                wait
-            elif [ $1 = 'office31' ]
+            if [ $1 = 'office31' ]
             then
                 CUDA_VISIBLE_DEVICES=$3 python target_finetune.py --dataset $1 \
                 --pretrain SHOT_LPFT --adapt $2 --few_shot $shot \
