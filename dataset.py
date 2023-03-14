0b813a0efdd5b3bbd22638cd0bcb786151c4f649
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2023-03-14 17:51:01 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,9 +3,27 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from sklearn.model_selection import train_test_split
-from PIL import ImageFile
-
+from PIL import ImageFile, Image
+import numpy as np
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+class SourceDomainImageFolder:
+    '''
+    Modified https://github.com/facebookresearch/DomainBed/blob/main/domainbed/datasets.py
+    src requires list of source domains in str type
+    '''
+    def __init__(self,root,src):
+        super().__init__()
+        assert type(src) == list
+        self.environments = src        
+        self.datasets = []
+        for environment in self.environments:
+            path = os.path.join(root, environment)
+            env_dataset = ImageFolder(path)
+
+            self.datasets.append(env_dataset)
+        
+        self.num_classes = len(self.datasets[-1].classes)
 
 class _SplitDataset(torch.utils.data.Dataset):
     """Used by split_dataset"""
@@ -22,15 +40,67 @@ class _SplitDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.keys)
 
-def split_dataset(dataset, ratio=0.2, seed=0):
+def make_dataset(image_list, labels):
+    if labels:
+      len_ = len(image_list)
+      images = [(image_list[i].strip(), labels[i, :]) for i in range(len_)]
+    else:
+      if len(image_list[0].split()) > 2:
+        images = [(val.split()[0], np.array([int(la) for la in val.split()[1:]])) for val in image_list]
+      else:
+        images = [(val.split()[0], int(val.split()[1])) for val in image_list]
+    return images
+
+def rgb_loader(path):
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+
+def l_loader(path):
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('L')
+
+class ImageList_idx(torch.utils.data.Dataset):
+    def __init__(self, image_list, labels=None, transform=None, target_transform=None, mode='RGB'):
+        imgs = make_dataset(image_list, labels)
+        if len(imgs) == 0:
+            raise(RuntimeError("Found 0 images in subfolders of: " + root + "\n"
+                               "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
+
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+        if mode == 'RGB':
+            self.loader = rgb_loader
+        elif mode == 'L':
+            self.loader = l_loader
+
+    def __getitem__(self, index):
+        path, target = self.imgs[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+    
+    def __len__(self):
+        return len(self.imgs)
+
+def split_dataset(dataset, args, ratio=0.2, seed=0):
     assert(ratio <= 0.5)
     keys = list(range(len(dataset)))
-    classes = dataset.targets
+    if args.subset or args.imbalance:
+        classes = [i[1] for i in dataset.imgs]
+    else:
+        classes = dataset.targets
     keys_1,keys_2 = train_test_split(keys,test_size=ratio,random_state=seed,stratify=classes)
+
     return keys_1, keys_2
 
 def get_transform(mode,type_='default'):
-    # TODO : define different type of augmentation
     if mode in ['val','test']:
         transform = transforms.Compose([
             transforms.Resize((224, 224)),
@@ -79,25 +149,95 @@ def few_shot_subset(targets,n_shot):
         idx = torch.where(targets==class_[i])[0]
         if count < n_shot+1:
             raise ValueError(f"Class {class_[i]} only have {count} samples, {n_shot}-Shot is not available")
+            # TODO : n_shot보다 적은 데이터수를 가진 클래스 어떻게 처리할지
+            # idx = idx[torch.randperm(len(idx))[:count+1]]
         else:
             temp = torch.randperm(len(idx))
+            # idx = idx[torch.randperm(len(idx))[:n_shot+1]]
+            # trn_idx, val_idx = idx[:-1], idx[-1]
             trn_idx, val_idx = idx[temp[:n_shot]], idx[temp[-1]]
         indices.extend(trn_idx.tolist())
         val_indices.append(val_idx)
-    
+
     return indices, val_indices
-    
-def get_target_dataset(args):
+
+def get_dataset(args):
+    '''
+    Returns source train/test datasets
+    '''   
     data_path = os.path.join(args.data_dir,args.dataset)
     domains = sorted([f.name for f in os.scandir(data_path) if f.is_dir()])
 
     tgt_domain = domains[args.target]
+    src_domains = [d for d in domains if d != tgt_domain] if args.source is None else [domains[args.source]]
+    
+    src_datasets = SourceDomainImageFolder(data_path,src_domains)
+    num_classes = src_datasets.num_classes
+
+    src_trn_datasets = []
+    src_tst_datasets = []
+    for dataset in src_datasets.datasets:
+        big_split,small_split = split_dataset(dataset,ratio=args.ratio,seed=args.seed)
+        big_split.transform = get_transform(mode='train',type_='default')
+        small_split.transform = get_transform(mode='test')
+        src_trn_datasets.append(big_split)
+        src_tst_datasets.append(small_split)
+        
+    return src_trn_datasets,src_tst_datasets,src_domains,num_classes
+
+    
+def get_target_dataset(args):
+    if args.dataset == 'Imagenet-C':
+        data_path = args.data_dir+'/'+args.dataset
+        domains = []
+        for f in os.scandir(data_path):
+            if f.is_dir() and f.name != 'extra':
+                for d in os.scandir(data_path+'/'+f.name):
+                    if d.is_dir():
+                        domains.append(f.name+'/'+d.name)
+        domains.sort()
+    else:
+        data_path = os.path.join(args.data_dir,args.dataset)
+        domains = sorted([f.name for f in os.scandir(data_path) if f.is_dir()])
+
+    tgt_domain = domains[args.target]
     print(tgt_domain)
      
-    path = data_path + f'/{tgt_domain}'
+    if args.dataset == 'Imagenet-C':
+        path = data_path + f'/{tgt_domain}/{args.severity}'
+    else:
+        path = data_path + f'/{tgt_domain}'
 
-    target_dataset = ImageFolder(os.path.join(path))
-    tr_idx, te_idx = split_dataset(target_dataset, ratio=args.ratio, seed=args.seed)
+    if args.subset:
+        if args.dataset == 'office_home':
+            args.class_num = 25
+            known_class = np.random.RandomState(seed=args.oda_seed).permutation(65)[:args.class_num]
+        if args.dataset == 'office31':
+            args.class_num = 10
+            known_class = np.random.RandomState(seed=args.oda_seed).permutation(31)[:args.class_num]
+        if args.dataset == 'VISDA-C':
+            args.class_num = 6
+            known_class = np.random.RandomState(seed=args.oda_seed).permutation(12)[:args.class_num]
+        txt_tar = open(f'/nas/home/tmddnjs3467/domain-generalization/SHOT/object/data/{args.dataset}/{tgt_domain}_list.txt').readlines()
+        label_dict = {known_class[i]:i for i in range(len(known_class))}
+        known_tar = []
+        t_classes = []
+        for i in range(len(txt_tar)):
+            rec = txt_tar[i]
+            reci = rec.strip().split(' ')
+            if int(reci[1]) in known_class:
+                line = reci[0] + ' ' + str(label_dict[int(reci[1])]) + '\n'   
+                known_tar.append(line)
+                t_classes.append(label_dict[int(reci[1])])
+        txt_known = known_tar.copy()
+        target_dataset = ImageList_idx(txt_known)
+    elif args.imbalance:
+        txt_tar = open(f'/nas/home/tmddnjs3467/domain-generalization/SHOT/object/data/{args.dataset}/{tgt_domain}_UT.txt').readlines()
+        target_dataset = ImageList_idx(txt_tar)
+    else:
+        target_dataset = ImageFolder(os.path.join(path))
+
+    tr_idx, te_idx = split_dataset(target_dataset, args, ratio=args.ratio, seed=args.seed)
     train_dataset, test_dataset = _SplitDataset(target_dataset, tr_idx), _SplitDataset(target_dataset, te_idx)
 
     train_dataset.transform = get_transform(mode='train',type_=args.aug)
@@ -105,16 +245,13 @@ def get_target_dataset(args):
 
     if args.few_shot:
         keys = list(range(len(train_dataset)))
-        targets = torch.tensor(train_dataset.underlying_dataset.targets)[train_dataset.keys]
+        if args.subset or args.imbalance:
+            targets = torch.tensor([target_dataset.imgs[i][1] for i in tr_idx])
+        else:
+            targets = torch.tensor(train_dataset.underlying_dataset.targets)[train_dataset.keys]
         assert len(keys) == len(targets)
-        
         few_shot_idx, val_idx = few_shot_subset(targets,args.few_shot) 
-        
-        # val_dataset = torch.utils.data.Subset(train_dataset,val_idx)
-        # tr_name = [target_dataset.samples[i][0] for i in tr_idx]
-        # val_name = [tr_name[i] for i in val_idx]
-        # import pdb;pdb.set_trace()
-        
+
         if args.robust:
             val_dataset = ImageFolder(os.path.join(f'./valset_cor_{args.dataset}/{args.seed}/{tgt_domain}'), transform=get_transform(mode='test'))
         else:
@@ -123,10 +260,13 @@ def get_target_dataset(args):
         
         # inspect few shot
         temp = []
-        for _, y in adapt_dataset:
+        for _,y in adapt_dataset:
             temp.append(y)
-        _, counts = torch.unique(torch.tensor(temp), return_counts=True)
-        assert torch.allclose(counts.float(), torch.ones(max(targets)+1)*args.few_shot)
+        _,counts = torch.unique(torch.tensor(temp),return_counts=True)
+        if args.subset:
+            assert torch.allclose(counts.float(),torch.ones(args.class_num)*args.few_shot)
+        else:
+            assert torch.allclose(counts.float(),torch.ones(max(targets)+1)*args.few_shot)
     else:
         adapt_dataset = train_dataset
-    return adapt_dataset, val_dataset, test_dataset, tgt_domain
+    return adapt_dataset,val_dataset,test_dataset,tgt_domain
